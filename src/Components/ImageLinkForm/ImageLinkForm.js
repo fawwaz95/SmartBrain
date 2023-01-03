9a5699a9b37abf3a0addbae5ffd05eccb6f36f8c
fawwaz95 <fawwaz_95@hotmail.com>
2023-01-03 16:49:39 -0500

    changed image link form css name to imglinkform, to avoid clashing with .form css in app.css

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
                 {'This Brain will detect faces in your pictures.'}
             </p>
             <div className='center'>
-                <div className='form center pa4 br3 shadow-5 ma4'>
+                <div className='imglinkform center pa4 br3 shadow-5 ma4'>
                     <input className='f4 pa2 w-70' type='text' onChange={onInputChange}/>
                     <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue' onClick={onButtonSubmit}> Detect </button>
                 </div>
